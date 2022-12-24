b61e40ad9137c5132110c6b6e458e8c8ab98f449
tranhuuthang2k <tranhuuthangcoi@gmail.com>
2022-12-24 21:51:14 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule("svg");
-
-    svgRule.uses.clear();
-
-    svgRule
-      .use("babel-loader")
-      .loader("babel-loader")
-      .end()
-      .use("vue-svg-loader")
-      .loader("vue-svg-loader");
-  },
-};
+  publicPath: undefined,
+  outputDir: '../public/site/dist',
+  assetsDir: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
